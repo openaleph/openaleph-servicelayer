1e3e9c7dbeaec1ad384ee35a2d6320ddafa15193
Simon Wörpel <simon.woerpel@pm.me>
2026-09-01 13:04:46 +0200

    Bump version: 1.24.0 → 1.25.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="openaleph-servicelayer",
-    version="1.24.0",
+    version="1.25.0",
     description="Basic remote service functions for openaleph components",
     classifiers=[
         "Development Status :: 3 - Alpha",
